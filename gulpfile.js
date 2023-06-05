aac07d1e5c9fccd29193341bda6e57037f315ee9
JDigar <jdigar96@gmail.com>
2023-06-05 21:05:55 +0000

    iniciando

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ const {
     dest,
     watch
 } = require("gulp");
-
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo de SASS
+        .pipe(plumber())
         .pipe(sass()) // Compilarlo
         .pipe(dest('build/css')); // Almacenarla en el disco duro 
 
