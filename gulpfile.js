5c7d5fb1e839f4144134a6cacdd06c06cfac464d
JDigar <jdigar96@gmail.com>
2023-06-12 21:16:43 +0000

    webp, avif

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const {
     src,
     dest,
-    watch
+    watch, 
+    parallel
 } = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
+
+//CSS
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//IMAGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif =require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo de SASS
@@ -15,10 +24,50 @@ function css(done) {
     done(); // callback que avisa a Gulp cuando llegamos al final de la funcion
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png, jpg}')
+        .pipe( cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+
+    done()
+}
+
+function versionWebp (done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png, jpg}')
+        .pipe (webp(opciones))
+        .pipe(dest('build/img'))
+
+    done ();
+}
+
+function versionAvif (done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png, jpg}')
+        .pipe (avif(opciones))
+        .pipe(dest('build/img'))
+
+    done ();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel (imagenes, versionWebp, versionAvif, dev);
